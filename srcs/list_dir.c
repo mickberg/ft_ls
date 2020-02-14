f2370955a595ed2318695f0aa9b823bded2c2229
mickberg <mikael.berglund2@gmail.com>
2020-02-14 18:11:24 +0200

    gets file name lengths

--- srcs/list_dir.c
+++ srcs/list_dir.c
@@ -6,24 +6,42 @@
 /*   By: mikaelberglund <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:01:13 by mikaelber         #+#    #+#             */
-/*   Updated: 2020/02/14 15:12:28 by mikaelber        ###   ########.fr       */
+/*   Updated: 2020/02/14 18:10:21 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_ls.h"
 
+static int	get_lengths(char *path, int opts)
+{
+	DIR				*dir;
+	struct dirent	*dp;
+	int				namelen;
+
+	namelen = 0;
+	dir = opendir(path);
+	while ((dp = readdir(dir)) != NULL)
+	{
+		if (!(opts & OFLAG_ALL) && dp->d_name[0] == '.')
+			continue;
+		namelen = ft_max(namelen, dp->d_namlen);
+	}
+	return (namelen);
+}
+
 void	list_dir(char *path, int opts)
 {
 	DIR				*dir;
 	struct dirent	*dp;
+	int				namelen;
 
+	namelen = get_lengths(path, opts);
 	dir = opendir(path);
-
 	while (( dp = readdir(dir)) != NULL)
 	{
 		if (!(opts & OFLAG_ALL) && dp->d_name[0] == '.')
 			continue;
-		ft_printf("%-10.*s", (int)dp->d_namlen, dp->d_name);
+		ft_printf("%-10.*s", namelen, dp->d_name);
 	}
 	(void)closedir(dir);
 }
